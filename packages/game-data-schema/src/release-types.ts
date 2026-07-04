9b64006bf2eaa3692df5e33aad434c710a6dfb28
Jeong-hwan Cho <36768979+chojh1027@users.noreply.github.com>
2026-07-04 13:12:16 +0900

    Define data release contract types

--- packages/game-data-schema/src/release-types.ts
+++ packages/game-data-schema/src/release-types.ts
@@ -1,1 +1,43 @@
-placeholder
+export type DataChunkKind = "common" | "faction";
+
+export interface ReleaseIndexEntry {
+  readonly id: string;
+  readonly effectiveDate: string;
+  readonly manifestPath: string;
+}
+
+export interface ReleaseIndex {
+  readonly schemaVersion: 1;
+  readonly gameSystem: "warhammer-40000";
+  readonly latestReleaseId: string;
+  readonly releases: readonly ReleaseIndexEntry[];
+}
+
+interface DataChunkDescriptorBase {
+  readonly id: string;
+  readonly path: string;
+  readonly sha256: string;
+  readonly sizeBytes: number;
+}
+
+export interface CommonDataChunkDescriptor extends DataChunkDescriptorBase {
+  readonly kind: "common";
+}
+
+export interface FactionDataChunkDescriptor extends DataChunkDescriptorBase {
+  readonly kind: "faction";
+  readonly factionId: string;
+}
+
+export type DataChunkDescriptor =
+  | CommonDataChunkDescriptor
+  | FactionDataChunkDescriptor;
+
+export interface ReleaseManifest {
+  readonly schemaVersion: 1;
+  readonly gameSystem: "warhammer-40000";
+  readonly releaseId: string;
+  readonly effectiveDate: string;
+  readonly publishedDate: string;
+  readonly chunks: readonly DataChunkDescriptor[];
+}
