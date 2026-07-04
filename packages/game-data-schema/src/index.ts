35ca502bf78c6dda6354620faa37f629c9e52465
Jeong-hwan Cho <36768979+chojh1027@users.noreply.github.com>
2026-07-04 13:13:08 +0900

    Export release contract APIs

--- packages/game-data-schema/src/index.ts
+++ packages/game-data-schema/src/index.ts
@@ -1,8 +1,22 @@
 export { parseGameDataCatalog } from "./validation";
+export {
+  assertReleaseManifestMatchesIndex,
+  parseReleaseIndex,
+  parseReleaseManifest,
+} from "./release-validation";
 export type {
   AbilityEffect,
   RerollPolicyKind,
 } from "./ability-effects";
+export type {
+  CommonDataChunkDescriptor,
+  DataChunkDescriptor,
+  DataChunkKind,
+  FactionDataChunkDescriptor,
+  ReleaseIndex,
+  ReleaseIndexEntry,
+  ReleaseManifest,
+} from "./release-types";
 export type {
   Ability,
   Alliance,
